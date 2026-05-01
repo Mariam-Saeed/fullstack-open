5184ec24c4a0c0b002de23f391b7bc09a0675d08
Mariam Saeed <mariamsaeed750@gmail.com>
2026-05-02 00:51:43 +0300

    course info 2.1

---
+++ part2/courseinfo/src/Components/Course.jsx
@@ -0,0 +1,21 @@
+import Content from "./Content";
+import Header from "./Header";
+// import Total from "./Total";
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      /> */}
+    </div>
+  );
+};
+
+export default Course;
